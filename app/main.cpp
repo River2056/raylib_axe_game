46d41a0183e066b00da6db7492b1050bbe76076b
Kevin_Tung__ <kevin.tung@hytechtech.com>
2022-10-29 02:30:05 +0800

    initial commit axe_game

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,15 @@
 #include "raylib.h"
 #include <iostream>
 
+static constexpr const int WIDTH = 1280;
+static constexpr const int HEIGHT = 960;
+
 int main() {
-  std::cout << "hello from raylib template!\n";
+  InitWindow(WIDTH, HEIGHT, "demo");
+  float rootBeer = 1.99;
+  double cheeseBurger = 5.99;
+  bool shouldHaveLunch = true;
+
+  std::cout << shouldHaveLunch << '\n';
   return 0;
 }
